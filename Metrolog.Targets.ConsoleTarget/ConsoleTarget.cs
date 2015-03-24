b22e174329878be5c63158d8ef867df22523315f
Daniel Kreuzhofer <daniel.kreuzhofer@live.com>
2015-03-24 13:47:50 +0100

    initial check in

---
+++ Metrolog.Targets.ConsoleTarget/ConsoleTarget.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetroLog;
+using MetroLog.Layouts;
+using MetroLog.Targets;
+
+namespace Metrolog.Targets.ConsoleTarget
+{
+    public class ConsoleTarget : SyncTarget
+    {
+        public ConsoleTarget()
+            : base(new SingleLineLayout())
+        { }
+
+        public ConsoleTarget(Layout layout) : base(layout)
+        {
+        }
+
+        protected override void Write(LogWriteContext context, LogEventInfo entry)
+        {
+            Console.WriteLine(Layout.GetFormattedString(context, entry));
+        }
+    }
+}
